fa2571f94d276fe07117f18f0d8a29e0af187202
desero <desero@desero.com>
2016-10-27 16:12:45 +0300

    Apply lint

--- bin/create.js
+++ bin/create.js
@@ -1,51 +1,50 @@
-var fs = require('fs');
-var cheerio = require('cheerio');
-var camelcase = require('camelcase');
-var capitalize = require('capitalize');
-var components = {};
-var types = {};
-var _ = require('underscore');
-var glob = require('glob');
-var path = require('path');
-var rootDir = path.join(__dirname, '..');
-var attrs = ['xlink:href', 'clip-path', 'fill-opacity', 'fill'];
-var cleanAtrributes = function($el, $) {
-    _.each(attrs, function(attr) {
-        $el.removeAttr(attr);
-    });
-    if($el.children().length === 0) {
-        return false;
-    }
+const fs = require('fs');
+const cheerio = require('cheerio');
+const camelcase = require('camelcase');
+const capitalize = require('capitalize');
+const _ = require('underscore');
+const glob = require('glob');
+const path = require('path');
 
-    $el.children().each(function(index, el) {
-        cleanAtrributes($(el), $);
-    });
+const components = {};
+const types = {};
+const rootDir = path.join(__dirname, '..');
+const attrs = ['xlink:href', 'clip-path', 'fill-opacity', 'fill'];
+const cleanAtrributes = function ($el, $) {
+  _.each(attrs, function (attr) {
+    $el.removeAttr(attr);
+  });
+  if ($el.children().length === 0) {
+    return false;
+  }
+
+  $el.children().each(function (index, el) {
+    cleanAtrributes($(el), $);
+  });
 };
 
-glob(rootDir + '/icons/particles/*.svg', function(err, icons) {
-    icons.forEach(function(iconPath){
-        var id = path.basename(iconPath, '.svg');
-        var svg = fs.readFileSync(iconPath, 'utf-8');
-        $ = cheerio.load(svg,{
-            xmlMode: true
-        });
-        var $svg = $('svg');
-        cleanAtrributes($svg, $);
-        var iconSvg = $svg.html();
-        var viewBox = $svg.attr('viewBox');
-        var folder = iconPath.replace(path.join(rootDir, 'icons') + '/', '').replace( '/' + path.basename(iconPath), '');
-        var type = capitalize(camelcase(folder));
-        var name = type + capitalize(camelcase(id));
-        var location = iconPath.replace(path.join(rootDir, 'icons'), '').replace('.svg', '.js');
-        components[name] = location;
-        if (!types[folder]) {
-            types[folder] = {};
-        }
-        types[folder][name] = location;
-        if (!fs.existsSync(folder)){
-          fs.mkdirSync(folder);
-        }
-        var component = `import React from 'react'
+glob(rootDir + '/icons/particles/*.svg', function (err, icons) {
+  icons.forEach(function (iconPath) {
+    const id = path.basename(iconPath, '.svg');
+    const svg = fs.readFileSync(iconPath, 'utf-8');
+    const $ = cheerio.load(svg, { xmlMode: true });
+    const $svg = $('svg');
+    cleanAtrributes($svg, $);
+    const iconSvg = $svg.html();
+    const viewBox = $svg.attr('viewBox');
+    const folder = iconPath.replace(path.join(rootDir, 'icons') + '/', '').replace('/' + path.basename(iconPath), '');
+    const type = capitalize(camelcase(folder));
+    const name = type + capitalize(camelcase(id));
+    const location = iconPath.replace(path.join(rootDir, 'icons'), '').replace('.svg', '.js');
+    components[name] = location;
+    if (!types[folder]) {
+      types[folder] = {};
+    }
+    types[folder][name] = location;
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder);
+    }
+    const component = `import React from 'react'
 import IconBase from 'react-icon-base'
 
 export const ${name} = (props) => {
@@ -58,19 +57,18 @@ export const ${name} = (props) => {
 
 export default ${name}
 
-`
-        fs.writeFileSync(path.join(rootDir, location), component, 'utf-8');
-        console.log(path.join('.', location));
-    });
-    _.each(types, function(components, folder) {
-        var iconsModule = _.map(components, function(loc, name){
-            loc = loc.replace('.js', '');
-            loc = loc.replace('/'+folder, '');
-            loc = "." + loc;
-            return `export { ${name} } from '${loc}';`;
-        }).join('\n') + '\n';
-        fs.writeFileSync(path.join(rootDir, folder , 'index.js'), iconsModule, 'utf-8');
-        console.log(path.join('.', folder, 'index.js'));
-    });
-    console.log("IconBase.js");
+`;
+    fs.writeFileSync(path.join(rootDir, location), component, 'utf-8');
+  });
+  _.each(types, function (cmps, folder) {
+    const iconsModule = _.map(cmps, function (locatio, name) {
+      let loc = locatio.replace('.js', '');
+      loc = loc.replace('/' + folder, '');
+      loc = '.' + loc;
+      return `
+import ${name} from '${loc}'
+export { ${name} }`;
+    }).join('\n') + '\n';
+    fs.writeFileSync(path.join(rootDir, folder, 'index.js'), iconsModule, 'utf-8');
+  });
 });
