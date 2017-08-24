9bd16196f56276b32684b68e7395bd585e53ae25
Vlad Temian <vladtemian@gmail.com>
2017-08-24 20:05:01 +0300

    Add link to github

--- icons/info.js
+++ icons/info.js
@@ -3,6 +3,7 @@ module.exports = {
     name: 'Presslabs Particles',
     url: 'http://presslabs.com/',
     attribution: 'A set of curated handmade icons to use with Presslabs.',
+    githubUrl: 'https://github.com/PressLabs/react-particles-icons',
     licence: 'MIT',
     licenceUrl: 'https://github.com/PressLabs/react-particles-icons/blob/master/LICENSE'
   }
