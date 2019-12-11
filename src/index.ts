cc8309dbe3dc7290aeff77b8c3ab7b4e0688956e
Mihai Grescenko <desero@desero.com>
2019-12-11 13:31:12 +0200

    Test compile

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,5 @@ import * as IconContents from "./generated/iconContents";
 import * as IconNames from "./generated/iconNames";
 
 export { IconContents, IconNames };
-export { IconSvgPaths32 } from "./generated/iconSvgPaths";
+export { IconSvgPaths20 } from "./generated/iconSvgPaths";
 export { IconName } from "./iconName";
